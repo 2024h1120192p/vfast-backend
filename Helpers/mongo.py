389828aa3c4764583b9af5830b4248bc80801676
Shreyas Ashtamkar <shreyu@programmer.net>
2025-02-13 01:43:26 +0530

    Respond with Email and phone

--- Helpers/mongo.py
+++ Helpers/mongo.py
@@ -403,6 +403,8 @@ def get_user_bookings_pipeline(user):
             'check_in': 1,
             'check_out': 1,
             'booked_room_type': 1,
+            'email':1,
+            'phone_number':1,
             'booking_status': 1,
             'pax': 1,
             'number_of_rooms': {
